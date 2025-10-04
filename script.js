49347a7754d61bc195fa2e3346eaa625ab9ba631
Vasto <sonicdarkultra44@gmail.com>
2025-10-04 07:25:44 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 // SCRIPT.JS
-console.log("wsg")
+console.log("canvas");
+let cnv = document.getElementById("game");
+let ctx = canvas.getContext("2d");
+
+const GAME_WIDTH = 1000, GAME_HEIGHT = 1000;
+[cnv.width, cnv.height] = [GAME_WIDTH, GAME_HEIGHT];
+
+requestAnimationFrame(drawBreakIt);
+function drawBreakIt() {
+  ctx.fillStyle = "rgb(200, 200, 200)";
+  ctx.fillRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
+  requestAnimationFrame(drawBreakIt);
+}
