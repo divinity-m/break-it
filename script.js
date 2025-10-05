13a2d53b80a35e47ce6d7a57704445874b5c863d
Vasto <sonicdarkultra44@gmail.com>
2025-10-05 08:42:43 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 // SCRIPT.JS
-console.log("canvas");
+console.log("finnaly a direction");
 let cnv = document.getElementById("game");
 let ctx = cnv.getContext("2d");
 
-const GAME_WIDTH = 800, GAME_HEIGHT = 700;
-[cnv.width, cnv.height] = [GAME_WIDTH, GAME_HEIGHT];
+window.addEventListener('resize', (e) => {
+  [cnv.width, cnv.height] = [e.target.innerWidth, e.target.innerHeight];
+})
 
 requestAnimationFrame(drawBreakIt);
 function drawBreakIt() {
   ctx.fillStyle = "rgb(200, 200, 200)";
-  ctx.fillRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
+  ctx.fillRect(0, 0, cnv.width, cnv.height);
+
+  ctx.fillStyle = "rgb(255, 255, 255)";
+  ctx.textAlign = "center"
+  ctx.font = "30px Arial";
+  ctx.fillText("You Reap What You Sow", cnv.width/2, 100);
+  
   requestAnimationFrame(drawBreakIt);
 }
